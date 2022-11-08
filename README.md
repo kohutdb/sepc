ff83155fdbb28d5dc64d2f43d8235d113ad40c8f
Davyd Kohut <davyd.kohut@gmail.com>
2022-11-08 14:01:32 +0200

    0.0.5

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # sepc
 
-Dead simple JSON-RPC over HTTP/WebSockets server for Node.js.
+Dead simple JSON-RPC over HTTP/WebSockets server for Node.js. Built with [jepc](https://github.com/kohutd/jepc)
+and [express](https://expressjs.com). If you
+need a client, try [jepc](https://github.com/kohutd/repc).
 
 ## Installation
 
--- src/main.js
+++ src/main.js
@@ -1,191 +1,12 @@
-import getParameterNames from "get-parameter-names";
 import express from "express";
 import { WebSocketServer } from 'ws';
+import jepc, { JsonRpcError as JepcRpcError } from 'jepc';
 
-export class JsonRpcError {
-    constructor(code, message, data = undefined) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
-}
-
-class SepcMethod {
-    constructor(fn, types = null) {
-        this.fn = fn;
-        this.types = types;
-    }
-
-    call(params) {
-        return this.fn(params);
-    }
-}
-
-export function method(fn, types = null) {
-    return new SepcMethod(fn, types);
-}
-
-function createMethodHandler(fn) {
-    if (fn instanceof SepcMethod) {
-        return (params) => fn.call(params);
-    }
-
-    const names = getParameterNames(fn);
-
-    return (params) => {
-        if (names[0] && (names[0].startsWith('{') || names[0].startsWith('['))) {
-            return fn(params);
-        }
-
-        if (!Array.isArray(params)) {
-            params = names.map((name) => params[name]);
-        }
-
-        return fn(...params);
-    };
-}
-
-function makeError(id, error) {
-    return {
-        jsonrpc: '2.0',
-        error,
-        id,
-    };
-}
-
-function makeResult(id, result) {
-    return {
-        jsonrpc: '2.0',
-        result,
-        id,
-    };
+export class JsonRpcError extends JepcRpcError {
 }
 
 function sepc(methods = {}, startFn = null) {
-    methods = { ...methods };
-
-    Object.entries(methods).forEach(([name, fn]) => {
-        methods[name] = createMethodHandler(fn);
-    });
-
-    async function call(request) {
-        // { "jsonrpc": "2.0", "method": "...", "params": [], "id": 1 }
-        if (!request || typeof request !== 'object') {
-            return makeError(null, {
-                code: -32600,
-                message: 'Invalid Request',
-            });
-        }
-
-        let { jsonrpc, method, params = [], id = null } = request;
-
-        // 2.0
-        if (jsonrpc !== '2.0') {
-            return makeError(id, {
-                code: -32600,
-                message: 'Invalid Request',
-            });
-        }
-
-        // "getUsers"
-        if (typeof method !== 'string') {
-            return makeError(id, {
-                code: -32600,
-                message: 'Invalid Request',
-            });
-        }
-
-        // {}, []
-        if (!params || typeof request !== 'object') {
-            return makeError(id, {
-                code: -32600,
-                message: 'Invalid Request',
-            });
-        }
-
-        // 1, 3.14, "ha.sh"
-        if (id && typeof id !== "string" && typeof id !== "number") {
-            return makeError(id, {
-                code: -32600,
-                message: 'Invalid Request',
-            });
-        }
-
-        if (!(method in methods)) {
-            return makeError(id, {
-                code: -32601,
-                message: 'Method not found'
-            });
-        }
-
-        try {
-            const result = await methods[method](params);
-
-            const madeResult = makeResult(id, result);
-
-            if (!id) {
-                return '';
-            }
-
-            return madeResult;
-        } catch (e) {
-            if (!id) {
-                return '';
-            }
-
-            if (e instanceof JsonRpcError) {
-                return makeError(id, {
-                    code: e.code,
-                    message: e.message,
-                    data: e.data,
-                });
-            } else {
-                return makeError(id, {
-                    code: -32603,
-                    message: 'Internal error',
-                });
-            }
-        }
-    }
-
-    async function handle(message, send) {
-        let request;
-
-        try {
-            request = JSON.parse(message);
-        } catch (e) {
-            return send(makeError(null, {
-                code: -32700,
-                message: 'Parse error'
-            }));
-        }
-
-        let output;
-
-        if (Array.isArray(request)) {
-            if (!request.length) {
-                return send(makeError(null, {
-                    code: -32600,
-                    message: 'Invalid Request',
-                }));
-            }
-
-            output = (await Promise.all(request.map((input) => call(input))))
-                .filter((v) => !!v);
-
-            if (!output.length) {
-                return send('');
-            }
-        } else {
-            output = await call(request);
-        }
-
-        if (output) {
-            return send(output);
-        } else {
-            return send('');
-        }
-    }
+    const { handle, methods } = jepc(methods);
 
     function listen(port = 3000, path = '/', callback = undefined) {
         const app = express();
@@ -193,13 +14,14 @@ function sepc(methods = {}, startFn = null) {
         app.use(express.text({ type: '*/*' }));
 
         app.post(path, (request, response) => {
-            handle(request.body, (data) => {
-                if (data) {
-                    response.send(data);
-                } else {
-                    response.send('');
-                }
-            });
+            handle(request.body)
+                .then((out) => {
+                    if (out) {
+                        response.send(out)
+                    } else {
+                        response.end();
+                    }
+                });
         });
 
         let wss;
@@ -209,11 +31,12 @@ function sepc(methods = {}, startFn = null) {
 
             wss.on('connection', (client) => {
                 client.on('message', (message) => {
-                    handle(message, (data) => {
-                        if (data) {
-                            client.send(JSON.stringify(data));
-                        }
-                    });
+                    handle(message)
+                        .then((out) => {
+                            if (out) {
+                                client.send(JSON.stringify(out));
+                            }
+                        });
                 });
             });
 
@@ -223,7 +46,6 @@ function sepc(methods = {}, startFn = null) {
         if (typeof startFn === "function") {
             return startFn({
                 methods,
-                call,
                 app,
                 port,
                 path,
@@ -246,7 +68,7 @@ function sepc(methods = {}, startFn = null) {
         return server;
     }
 
-    return { listen, call, methods };
+    return { listen, handle, methods };
 }
 
 sepc.ws = (methods, startFn = null) => sepc(methods, { asWs: true, startFn });
